e99365c03dbdfe44be93ed2c9129c89a5ee8d975
axelreichwein <axel.reichwein@koneksys.com>
2015-12-11 16:54:03 -0800

    - added license clause to client code

--- edu.gatech.mbsec.adapter.magicdraw.clients/src/main/java/tdb/sparql/clients/QueryTriplestoreForMagicDrawResourcesOfSpecificModel.java
+++ edu.gatech.mbsec.adapter.magicdraw.clients/src/main/java/tdb/sparql/clients/QueryTriplestoreForMagicDrawResourcesOfSpecificModel.java
@@ -1,3 +1,18 @@
+/*********************************************************************************************
+ * Copyright (c) 2015 Model-Based Systems Engineering Center, Georgia Institute of Technology.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ *  
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Eclipse Distribution License is available at
+ *  http://www.eclipse.org/org/documents/edl-v10.php.
+ *  
+ *  Contributors:
+ *  
+ *	   Axel Reichwein (axel.reichwein@koneksys.com)		- initial implementation       
+ *******************************************************************************************/
 package tdb.sparql.clients;
 
 import java.io.InputStream;
